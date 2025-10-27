aa2ce6157ec2d7e88e6f7b772b8a31b752925e88
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-10-27 11:17:13 -0600

    helping out

--- Chess/pieces.py
+++ Chess/pieces.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class Piece(ABC):
-    def __init__(self, colour, position):
+    def __init__(self, colour, position, type):
         self.colour = colour
         self.position = position
+        self.type = type
 
     def get_position(self):
         return self.position
@@ -15,77 +17,210 @@ class Piece(ABC):
     def get_symbol(self):
         pass
 
+class ChessGame():
+    def __init__(self, white, black):
+        self.white = white
+        self.black = black
+
+    def move_piece(self, new_pos, white, black):
+        if self.type == "pawn":
+            Pawn.move(self, self.position, new_pos, white, black)
+        elif self.type == "knight":
+            Knight.move(self, self.position, new_pos, white, black)
+        elif self.type == "rook":
+            Rook.move(self, self.position, new_pos, white, black)
+        elif self.type == "bishop":
+            Bishop.move(self, self.position, new_pos, white, black)
+        elif self.type == "king":
+            King.move(self, self.position, new_pos, white, black)
+        elif self.type == "queen":
+            Queen.move(self, self.position, new_pos, white, black)
+
+    def piece_pos(self):
+        print(f"{self.type} at {self.position}")
+
+    def pieces_left(white, black, colour):
+        if colour == "white":
+            for i in white:
+                print(f"{i.type.capitalize()} at: {i.position}")
+        elif colour == "black":
+            for i in black:
+                print(f"{i.type.capitalize()} at: {i.position}")
+    
+    def remove_piece(self, white, black):
+        if self.colour == "white":
+            print(f"Removing {self.type} at {self.position}")
+            white.remove(self)
+
+        elif self.colour == "black":
+            black.remove(self)
+        return
+    
+    def check_space(self, black, white, new_pos):
+        print("In space")
+        if self.colour == "white":
+            for i in black:
+                print(i, i.colour, i.position)
+                print(self, self.colour, new_pos)
+                if i.position == new_pos:
+                    print(f"You have captured a {i.colour} {i.type} at {i.position}")
+                    black.remove(i)
+                    return True
+        elif self.colour == "black":
+            for i in white:
+                if i.position == new_pos:
+                    print(f"You have captured a {i.colour} {i.type} at {i.position}")
+                    white.remove(i)
+                    return True
+        return False
+    
+    def check_pos(position, new_pos):
+        print("In pos")
+        if (1 < position[0] > 8) or (1 < position[1] > 8):
+            return False
+        else: return True
+
+    def find_piece(white, black):
+        pie = list(map(int, input("What's the row and column of the piece, seperated by a space?\n\n    ").split()))
+        for i in white:
+            if i.position == pie:
+                print(f"Your piece is {i.colour}, a {i.type} and is at {i.position}")
+                ch = i
+        for i in black:
+            if i.position == pie:
+                print(f"Your piece is {i.colour}, a {i.type} and is at {i.position}")
+                ch = i
+        if ch:
+            if int(input("1 if this is correct:\n\n    ")) == 1:
+                return ch
+    
+    def show_board(white, black, display_base):
+        temp = display_base
+        f = 0
+        print("In")
+        for i in white:
+            print(i)
+            if i.position[0] > 1:
+                for a in temp:
+                    f += 1
+                    if f == (i.position[0]*15 + i.position[1]*2 +1):
+                        print(i.position[0]*15 + i.position[1]*2 +1)
+                        print(a)
+                    
+            elif i.position[0] == 1:
+                for a in temp:
+                    f += 1
+                    if f == (i.position[0]*15 + i.position[1]*2):
+                        print("Row 1")
+                        print(i.position[0]*15 + i.position[1]*2)
+                        print(a)
+
+
 
 class Pawn(Piece):
-    def __init__(self, colour, position):
-        super().__init__(colour, position)
+    def __init__(self, colour, position, type):
+        super().__init__(colour, position, type)
 
-    def move(self, position, new_position):
+    def move(self, position, new_position, white, black):
         if (self.colour == "white" and new_position == [(position[0]+1),position[1]]) or (self.colour == "black" and new_position == [(position[0]-1),position[1]]):
-            self.position = new_position
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
         elif (self.colour == "white" and (new_position == [(position[0]+1),(position[1]+1)]) or (new_position == [(position[0]+1),(position[1]-1)])) or (self.colour == "black" and (new_position == [(position[0]-1),(position[1]+1)]) or (new_position == [(position[0]-1),(position[1]-1)])):
-            self.position = new_position
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
+        print("Invalid move for pawn!")
+        return
 
     def get_symbol(self):
         return '♙' if self.colour == 'black' else '♟'
     
 class Rook(Piece):
-    def __init__(self, colour, position):
-        super().__init__(colour, position)
+    def __init__(self, colour, position, type):
+        super().__init__(colour, position, type)
 
-    def move(self, position, new_position):
+    def move(self, position, new_position, white, black):
         if (position[0] == new_position[0]) or (position[1] == new_position[1]):
-            self.position = new_position
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
+        print("Invalid move for rook!")
+        return
 
     def get_symbol(self):
         return '♖' if self.colour == 'black' else '♜'
     
 class Knight(Piece):
-    def __init__(self, colour, position):
-        super().__init__(colour, position)
+    def __init__(self, colour, position, type):
+        super().__init__(colour, position, type)
 
-    def move(self, position, new_position):
+    def move(self, position, new_position, white, black):
         if (new_position == [(position[0]+2),(position[1]+1)]) or (new_position == [(position[0]+2),(position[1]-1)]) or (new_position == [(position[0]-2),(position[1]+1)]) or (new_position == [(position[0]-2),(position[1]-1)]) or (new_position == [(position[0]+1),(position[1]+2)]) or (new_position == [(position[0]+1),(position[1]-2)]) or (new_position == [(position[0]-1),(position[1]+2)]) or (new_position == [(position[0]-1),(position[1]-2)]):
-            self.position = new_position
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
+        print("Invalid move for knight!")
+        return
     
     def get_symbol(self):
         return '♘' if self.colour == 'black' else '♞'
     
 class Bishop(Piece):
-    def __init__(self,colour,position):
-        super().__init__(colour,position)
+    def __init__(self,colour,position, type):
+        super().__init__(colour,position, type)
 
-    def move(self,position,new_position):
+    def move(self,position,new_position, white, black):
         for i in range(8):
             if (new_position == [(position[0]+i),(position[1]+i)]) or (new_position == [(position[0]-i),(position[1]+i)]) or (new_position == [(position[0]+i),(position[1]-i)]) or (new_position == [(position[0]-i),(position[1]-i)]):
-                self.position = new_position
+                if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                    self.position = new_position
+                    print("Moved!, pos:", self.position)
+                    return
+            print("Invalid move for bishop!")
+            return
 
     def get_symbol(self):
         return '♗' if self.colour == 'black' else '♝'
     
 class Queen(Piece):
-    def __init__(self,colour,position):
-        super().__init__(colour,position)
+    def __init__(self,colour,position, type):
+        super().__init__(colour,position, type)
 
-    def move(self,position,new_position):
+    def move(self,position,new_position, white, black):
         for i in range(8):
             if (new_position == [(position[0]+i),(position[1]+i)]) or (new_position == [(position[0]-i),(position[1]+i)]) or (new_position == [(position[0]+i),(position[1]-i)]) or (new_position == [(position[0]-i),(position[1]-i)]):
-                self.position = new_position
+                if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                    self.position = new_position
+                    print("Moved!, pos:", self.position)
+                    return
         if (position[0] == new_position[0]) or (position[1] == new_position[1]):
-            self.position = new_position
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
+        print("Invalid move for queen!")
+        return
 
     def get_symbol(self):
         return '♕' if self.colour == 'black' else '♛'
 
 class King(Piece):
-    def __init__(self,colour,position):
-        super().__init__(colour,position)
+    def __init__(self,colour,position, type):
+        super().__init__(colour,position, type)
 
-    def move(self,position,new_position):
+    def move(self,position,new_position, white, black):
         if (new_position == [(position[0]+1),(position[1])]) or (new_position == [(position[0]-1),(position[1])]) or (new_position == [(position[0]),(position[1]+1)]) or (new_position == [(position[0]),(position[1]-1)]) or (new_position == [(position[0]+1),(position[1]+1)]) or (new_position == [(position[0]+1),(position[1]-1)]) or (new_position == [(position[0]-1),(position[1]+1)]) or (new_position == [(position[0]-1),(position[1]-1)]):
-            self.position = new_position
-            return
+            if ChessGame.check_pos(self.position, new_position) and ChessGame.check_space(self, black, white, new_position) is True:
+                self.position = new_position
+                print("Moved!, pos:", self.position)
+                return
         print("Invalid move for King")
+        return
 
     def get_symbol(self):
         return '♔' if self.colour == 'black' else '♚'
--- Chess/play.py
+++ Chess/play.py
@@ -1,5 +1,39 @@
 from pieces import *
 
+'''
+Requirements:
+    Follow the provided class diagram exactly 
+    Implement ChessPiece as an abstract class 
+    Create all six concrete piece classes (Pawn, Rook, Knight, Bishop, Queen, King) 
+    Implement canMoveTo(), getSymbol() methods 
+    Create ChessGame class with whitePieces and blackPieces lists 
+    Implement movePiece(), removePiece(), getPiecesLeft(), and getPieceAt() in ChessGame 
+    Create correct number of pieces for each player 
+    Set up pieces in starting positions 
+    Demonstrate moving 5 different pieces 
+    Implement basic move validation for each piece type 
+    Use removePiece() method for capturing 
+    Put all classes on 1 file separate from your running file 
+    Add comments to explain your code 
+    Test each piece type for correct movement 
+    Focus on core functionality over advanced game logic
+
+'''
+
+# ■  □  ▢
+# Each row is 15 chars, 8 blocks, 7 spaces
+# First row is always 1 more than the others, (16 regularly)
+#display_base = "■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■"
+#a= 0
+# [2,1] == a= 19
+# [3,1] == 35
+'''
+for i in display_base:
+    a += 1
+    if a == 16:
+        print(i, "okokd")
+print("Done")
+'''
 board = [
          [1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],
          [2,1],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],
@@ -12,10 +46,66 @@ board = [
 
 
 def main():
-    white_king = King('white', [0,4])
-    white_king.move(white_king.position, [1,4])
-    print(white_king.position)
-    white_king.move(white_king.position, [4,4])
-    print(white_king.position)
+    display_base = "■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■\n■ □ ■ □ ■ □ ■ □\n□ ■ □ ■ □ ■ □ ■"
+    white = [(white_pawn1 := Pawn('white', [2,1], 'pawn')),(white_pawn2 := Pawn('white', [2,2], 'pawn'))]
+    black = [(black_pawn1 := Pawn('black', [3,1], 'pawn')), (black_pawn2 := Pawn('black', [7,2], 'pawn'))]
+
+    ChessGame.show_board(white, black, display_base)
+
+    choice = input('''1. Move Piece
+2. Check Pieces
+3. Get Position of Piece
+4. Get Piece Symbol
+5. Show board
+5. Exit
+    
+    ''')
+    for i in white:
+        print(i.position, i.type, i.colour)
+    for i in black:
+        print(i.position, i.type, i.colour)
+    if choice == "1":
+        piece = ChessGame.find_piece(white, black)
+        ie = list(map(int, input("What's the row and column of the move you want to do, seperated by a space?\n\n    ").split()))
+        try:
+            ChessGame.move_piece(piece, ie, white, black)
+            return main()
+        except:
+            print("Invalid input/move")
+            return main()
+    elif choice == "2":
+        try:
+            ChessGame.pieces_left(white, black, input("What's your colour?\n\n    ".lower().strip()))
+            return main()
+        except:
+            print("Error in finding pieces left")
+            return main()
+    elif choice == "3":
+        try:
+            typ = input("What's the type of your piece?")
+            for i in black:
+                if i.colour == typ.lower().strip():
+                    ChessGame.piece_pos(i)  
+            for i in white:
+                if i.colour == typ.lower().strip():
+                    ChessGame.piece_pos(i)
+            return main()
+        except:
+            print("Invalid input!")
+            return main()
+    elif choice == "4":
+        pi = ChessGame.find_piece(white, black)
+        try:
+            print(pi.get_symbol)
+            return main()
+        except:
+            print("Error in finding character!")
+            return main()
+    elif choice == "5":
+        return
+    else:
+        print("Invalid choice!")
+        return main()
+    
 
 main()
